7714216c67521657efce42e51bc1d97348744cf6
LiamMcKinney <ljmckinney02@gmail.com>
2023-09-29 11:33:01 -0500

    Added comments to AbstractionLayer & DataTypes

--- AbstractionLayer.py
+++ AbstractionLayer.py
@@ -1,32 +1,55 @@
+"""
+AbstractionLayer.py
+
+Singleton class with observable events that systems can subscribe to and publish.
+
+Created by Liam McKinney
+
+Created 9/20/2023
+Revised 9/29/2023
+    -Added comments (Liam McKinney)
+"""
+
 from typing import TypeVar, Generic, List, Callable
 from DataTypes import *
 
+# We use type generic type hints for events to make it easier to
+# know what data types to expect during development
 T = TypeVar('T')
-
 class Event(Generic[T]):
-    subscribers: List[Callable[[T],None]]
+    subscribers: List[Callable[[T],None]] # list of callbacks when this event triggers
+
+    # basic initialization
     def __init__(self):
         self.subscribers = []
 
+    # trigger the event, calling all subscriber callbacks
     def trigger(self, data: T) -> None:
         for subCallBack in self.subscribers:
             subCallBack(data)
 
+    # provide a callback to be called whenever the event triggers
     def subscribe(self, callback: Callable[[T], None]) -> None:
         self.subscribers.append(callback)
 
+# singleton class to hold all game events
 class AbstractionLayer:
+    #make sure all constructor calls return the same instance
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(AbstractionLayer, cls).__new__(cls)
         return cls.instance
 
+    #initialize events
     def __init__(self) -> None:
+        #Robot commands
         self.playCard = Event[int]()
         self.drawCard = Event[None]()
 
+        # Game events - Robot's actions
         self.drewCard = Event[Card]()
         self.playedCard = Event[None]()
 
+        # Player actions
         self.oppDrew = Event[None]()
         self.oppPlayed = Event[Card]()
--- DataTypes.py
+++ DataTypes.py
@@ -1,11 +1,22 @@
+"""
+DataTypes.py
+
+file to hold any custom data types/classes we define
+
+Created by Liam McKinney
+
+Created 9/20/2023
+"""
 from enum import Enum
 
+# Enum representing the suit of a card
 class Suit(Enum):
     CLUBS = 1
     HEARTS = 2
     SPADES = 3
     DIAMONDS = 4
 
+# Enum representing the face value of a card
 class Value(Enum):
     Val_A = 1
     Val_2 = 2
@@ -21,6 +32,7 @@ class Value(Enum):
     Val_Q = 12
     Val_K = 13
 
+# class to represent a card from the deck
 class Card:
     suit: Suit
     val: Value
