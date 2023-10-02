c04a21dfb292757bce38e59c05f99ab2cf503503
Shelby Jones <shelbyj605@gmail.com>
2023-10-02 17:55:09 -0500

    Update GameStrategy.py

--- GameStrategy.py
+++ GameStrategy.py
@@ -7,15 +7,20 @@ Created by Shelby Jones
 
 Created 10/2/2023
 Revised 10/X/2023
-    -
+    -added bones
 """
 
 from DataTypes import *
 
 
-NaoCards = ["""list of cards"""]
+NaoCards = ["""list of cards"""] #an array to hold the current cards in Nao's hand
+PlayableCards = [] #an array for the playable cards for the current state
+FaceUpCard = Card
+
 
 def makeMove:
+    when(oppPlayed):
+
     #Nao Assesses current face-up card
     #Nao makes play decision
     #Nao makes game action, either playing or passing
