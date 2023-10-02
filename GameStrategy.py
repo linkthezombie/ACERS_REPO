9a1394df27f7e191f0eea081f10ccd2d52e17ca3
Shelby Jones <shelbyj605@gmail.com>
2023-10-02 17:45:22 -0500

    Create GameStrategy.py
    add requirement 16.1, game strategy interface

---
+++ GameStrategy.py
@@ -0,0 +1,26 @@
+"""
+GameStrategy.py
+
+Document to define how the game is played given Nao's current state
+
+Created by Shelby Jones
+
+Created 10/2/2023
+Revised 10/X/2023
+    -
+"""
+
+from DataTypes import *
+
+
+NaoCards = ["""list of cards"""]
+
+def makeMove:
+    #Nao Assesses current face-up card
+    #Nao makes play decision
+    #Nao makes game action, either playing or passing
+    #Nao Ends turn
+    
+
+
+
