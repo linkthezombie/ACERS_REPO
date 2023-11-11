088e71d0101666f7947d43211ec44757d3100b84
red2fred2 <red2fred2base@live.com>
2023-11-11 03:27:53 -0600

    Add unit tests to Vector3D Sure, these don't really need tests, but I'm going to need them to deal with quaternions. I'm getting used to python testing with something familiar.

---
+++ positioning/test_Vector3D.py
@@ -0,0 +1,46 @@
+from positioning.Vector3D import Vector3D
+
+def test_addition():
+    v0 = Vector3D([0.0, 0.0, 0.0])
+    v1 = Vector3D([1.0, 2.0, 3.0])
+    v2 = Vector3D([2.0, 0.0, 1.0])
+    v3 = Vector3D([-1.0, -1.0, -1.0])
+
+    assert v0 + v1 == v1
+    assert v0 + v2 == v2
+    assert v1 + v2 == Vector3D([3.0, 2.0, 4.0])
+    assert v1 + v3 == Vector3D([0.0, 1.0, 2.0])
+    assert v2 + v3 == Vector3D([1.0, -1.0, 0.0])
+
+def test_addition_identity():
+    zeroes = Vector3D([0, 0, 0])
+    assert zeroes + zeroes == zeroes
+
+def test_length():
+    assert Vector3D([0.0, 0.0, 0.0]).getLength() == 0.0
+    assert Vector3D([3.0, 4.0, 0.0]).getLength() == 5.0
+    assert Vector3D([-3.0, -4.0, -0.0]).getLength() == 5.0
+
+def test_scale():
+    bigNumber = 9999999999999999999999.0
+
+    v = Vector3D([0.0, 0.0, 0.0])
+    v.scale(bigNumber)
+    assert v.getLength() == 0.0
+
+    v = Vector3D([bigNumber, bigNumber, bigNumber])
+    v.scale(0.0)
+    assert v.getLength() == 0.0
+
+    v = Vector3D([3.0, 4.0, 0.0])
+    v.scale(1.0)
+    assert v.getLength() == 5.0
+    v.scale(1.0 / 5.0)
+    assert v.getLength() == 1.0
+    v.scale(100.0)
+    assert v.getLength() == 100.0
+
+def test_normalize():
+    v = Vector3D([123.0, 55555555.0, 0.0002])
+    v.normalize()
+    assert v.getLength() == 1.0
