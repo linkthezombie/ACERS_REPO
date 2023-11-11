c2b9ded93c5f591c931eabe8f523cd02b703e2ee
red2fred2 <red2fred2base@live.com>
2023-11-11 03:26:12 -0600

    Add length, scale, and normalize to Vector3D

--- positioning/Vector3D.py
+++ positioning/Vector3D.py
@@ -9,6 +9,7 @@ Created 11/11/2023
 """
 
 from dataclasses import dataclass
+from math import sqrt
 
 # Just makes sure values are all in the right places
 # the NAOqi axes make a right handed coordinate system with
@@ -33,9 +34,28 @@ class Vector3D:
 
         return Vector3D([x, y, z])
 
+    # Gets the length from tail to tip
+    def getLength(self) -> float:
+        x = self.x**2
+        y = self.y**2
+        z = self.z**2
+
+        return sqrt(x + y + z)
+
     # Gets the vector as a list of floats
     def getList(self) -> list[float]:
         return [self.x, self.y, self.z]
+
+    # Normalizes the vector so the length is 1
+    def normalize(self):
+        length = self.getLength()
+        self.scale(1 / length)
+
+    # Scales this vector by a scalar value
+    def scale(self, scalar: float):
+        self.x = self.x * scalar
+        self.y = self.y * scalar
+        self.z = self.z * scalar
 
     # Swizzles values to switch the coordinate system's handedness around the Z axis
     # I swear I didn't just make up a word https://en.wikipedia.org/wiki/Swizzling_(computer_graphics)
