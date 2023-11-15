fa9050499eb73b7b4f677529755c7b78ce3d8c4f
red2fred2 <red2fred2base@live.com>
2023-11-15 12:58:48 -0600

    Remove type hinting from Vector3D

--- positioning/Vector3D.py
+++ positioning/Vector3D.py
@@ -9,7 +9,6 @@ Created 11/11/2023
 """
 
 from copy import copy, deepcopy
-from dataclasses import dataclass
 from math import sqrt
 
 # Just makes sure values are all in the right places
@@ -17,34 +16,43 @@ from math import sqrt
 #     +x: out the front of the robot
 #     +y: out the left of the robot
 #     +z: up
-@dataclass
 class Vector3D:
-    x: float
-    y: float
-    z: float
+    # self.x: float
+    # self.y: float
+    # self.z: float
 
     # Constructs from a 3 vec
-    def __init__(self, vector: list[float]):
+	#
+	# vector: list[float], [x, y, z]
+    def __init__(self, vector):
         self.x, self.y, self.z = vector
 
     # Vector addition
+	#
+	# other: Vector3D, the RHS vector
+	# returns a new added Vector3D
     def __add__(self, other):
-        x: float = self.x + other.x
-        y: float = self.y + other.y
-        z: float = self.z + other.z
+        x = self.x + other.x
+        y = self.y + other.y
+        z = self.z + other.z
 
         return Vector3D([x, y, z])
 
     # Vector addition
+	#
+	# other: Vector3D, the RHS vector
+	# returns a new subtracted Vector3D
     def __sub__(self, other):
-        x: float = self.x - other.x
-        y: float = self.y - other.y
-        z: float = self.z - other.z
+        x = self.x - other.x
+        y = self.y - other.y
+        z = self.z - other.z
 
         return Vector3D([x, y, z])
 
     # Gets the length from tail to tip
-    def getMagnitude(self) -> float:
+	#
+	# returns a float
+    def getMagnitude(self):
         x = self.x**2
         y = self.y**2
         z = self.z**2
@@ -52,16 +60,23 @@ class Vector3D:
         return sqrt(x + y + z)
 
     # Gets the vector as a list of floats
-    def getList(self) -> list[float]:
+	#
+	# returns list[float]
+    def getList(self):
         return [copy(self.x), copy(self.y), copy(self.z)]
 
     # Returns a copy of this vector with a length of 1
+	#
+	# returns a new normalized Vcetor3D
     def normalize(self):
         length = self.getMagnitude()
         return self.scale(1.0 / length)
 
     # Returns a copy of this vector scaled by a scalar
-    def scale(self, scalar: float):
+	#
+	# scaler: float
+	# returns a new scaled Vector3D
+    def scale(self, scalar):
         x = self.x * scalar
         y = self.y * scalar
         z = self.z * scalar
@@ -71,6 +86,8 @@ class Vector3D:
     # Returns a copy of this vector with values swizzled to switch the coordinate
     # system's handedness around the Z axis.
     # I swear I didn't just make up a word https://en.wikipedia.org/wiki/Swizzling_(computer_graphics)
+	#
+	# returns a new swizzled Vector3D
     def swizzleHandedness(self):
         this = deepcopy(self)
         (this.x, this.y) = (self.y, self.x)
