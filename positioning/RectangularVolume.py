6c04ab91426326643aca2ffe6d985a95bf059784
red2fred2 <red2fred2base@live.com>
2023-11-16 23:47:55 -0600

    Remove type hinting in RectangularVolume

--- positioning/RectangularVolume.py
+++ positioning/RectangularVolume.py
@@ -8,24 +8,26 @@ Created by Dylan Polson
 Created 11/14/2023
 """
 
-from dataclasses import dataclass
-
 from positioning.Pose import Pose
 from positioning.Vector3D import Vector3D
 
 # A simple rectangular prism with some pose
 # The pose should be in the center of the volume in xy, sitting at the bottom in z
 class RectangularVolume:
-    pose: Pose
-    size: Vector3D
+    # pose: Pose
+    # size: Vector3D
 
-    def __init__(self, pose: Pose, size: Vector3D):
+	# Initialize a new volume with a size at a given pose
+	#
+	# pose: Pose, The bottom center of the volume
+	# size: Vector3D, the full dimensions of the volume
+    def __init__(self, pose, size):
         self.pose = pose
         self.size = size
 
     # Checks to see if a given pose is inside this volume
-    def contains(self, pose: Pose) -> bool:
-        correctedPose: Pose = pose.changeOrigin(self.pose)
+    def contains(self, pose):
+        correctedPose = pose.changeOrigin(self.pose)
 
         if correctedPose.position.x > self.size.x / 2.0:  return False
         if correctedPose.position.x < -self.size.x / 2.0: return False
