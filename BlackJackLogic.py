eb19adf8454724b74525d7f99f27a5840721896c
EliseLovell <112564745+EliseLovell@users.noreply.github.com>
2024-04-02 12:26:44 -0500

    Create BlackJackLogic.py
    code for black jack logic goes here

---
+++ BlackJackLogic.py
@@ -0,0 +1,22 @@
+"""
+BlackJackLogic.py
+
+Thinking required for Nao to play BlackJack
+
+Created 4/2/2024
+"""
+#!/usr/bin/env python2.7
+
+import AbstractionLayer
+
+absLayer = AbstractionLayer.AbstractionLayer()
+
+def startGame():
+  print("Starting game")
+  
+def turn():
+  print("decide to hit or pass")
+  
+def endGame():
+  print("did we lose or win?")
+
