60a2ecf6ea720c5fea3703eb6a83484e350bb97d
red2fred2 <red2fred2base@live.com>
2024-04-03 14:36:35 -0500

    Revert "Change Card construction from if statements to match statements"
    This reverts commit 0a566c0b3d08213ee582a782d026560e50e30715.

--- hand.py
+++ hand.py
@@ -132,41 +132,58 @@ class Card():
     def __ne__(self, other):
         return not (self == other)
 
-    #sets int for suit based on inputted string version of suit
     def setSuit(self, s):
-        match s.lower():
-            case "club" | "0":
-                self.suit = 1
-                self.ss = "club"
-            case "diamond" | "3":
-                self.suit = 2
-                self.ss = "diamond"
-            case "heart" | "1":
-                self.suit = 3
-                self.ss = "heart"
-            case "spade" | "2":
-                self.suit = 4
-                self.ss = "spade"
+        self.ss = s.lower()
+        #sets int for suit based on inputted string version of suit
+        if s.lower() == "club" or s == "0":
+            self.suit = 1
+            self.ss = "club"
+        elif s.lower() == "diamond" or s == "3":
+            self.suit = 2
+            self.ss = "diamond"
+        elif s.lower() == "heart" or s == "1":
+            self.suit = 3
+            self.ss = "heart"
+        elif s.lower() == "spade" or s == "2":
+            self.suit = 4
+            self.ss = "spade"
 
-    #sets int for value based on inputted string version of value
     def setValue(self, v):
+        self.vs = v.lower()
+        #sets string value to lowercase
+        v = v.lower()
 
-        match v.lower():
-            case "a" | "1":
-                self.value = 1
-                self.vs = "a"
-            case "j" | "11":
-                self.value = 11
-                self.vs = "j"
-            case "q" | "12":
-                self.value = 12
-                self.vs = "q"
-            case "k" | "13":
-                self.value = 13
-                self.vs = "k"
-            case default:
-                self.value = int(v)
-                self.vs = v
+        #sets int for value based on inputted string version of value
+        if v == "a" or v == "1":
+            self.value = 1
+            self.vs = "a"
+        elif v == "2" or v == "2":
+            self.value = 2
+        elif v == "3" or v == "3":
+            self.value = 3
+        elif v == "4" or v == "4":
+            self.value = 4
+        elif v == "5" or v == "5":
+            self.value = 5
+        elif v == "6" or v == "6":
+            self.value = 6
+        elif v == "7" or v == "7":
+            self.value = 7
+        elif v == "8" or v == "8":
+            self.value = 8
+        elif v == "9" or v == "9":
+            self.value = 9
+        elif v == "10" or v == "10":
+            self.value = 10
+        elif v == "j" or v == "11":
+            self.value = 11
+            self.vs = "j"
+        elif v == "q" or v == "12":
+            self.value = 12
+            self.vs = "q"
+        elif v == "k" or v == "13":
+            self.value = 13
+            self.vs = "k"
 
 #test functions for each method - result in output to console
 '''addCard( "2", "spade")
