0a8a4f9c49cf16d1218cc6d97b3b1e8bd864bdf9
EliseLovell <112564745+EliseLovell@users.noreply.github.com>
2024-04-03 13:02:19 -0500

    Update BlackJackLogic.py

--- BlackJackLogic.py
+++ BlackJackLogic.py
@@ -4,19 +4,59 @@ BlackJackLogic.py
 Thinking required for Nao to play BlackJack
 
 Created 4/2/2024
+  Edited 4/3/2024 - Elise Lovell
+     - added basic funcitonality on turn
 """
 #!/usr/bin/env python2.7
 
 import AbstractionLayer
+import hand
 
 absLayer = AbstractionLayer.AbstractionLayer()
 
 def startGame():
   print("Starting game")
-  
-def turn():
-  print("decide to hit or pass")
-  
-def endGame():
-  print("did we lose or win?")
+
+#abs layer subscribe
+def hitOrPass():
+  if totalHand() >= 17:
+    print("I pass")
+    #abs layer call
+else:
+  print("Hit")
+  #abs layer call
+
+#abs layer subscribe
+#add a newly draw card to Nao's hand
+def getNewCard(card):
+  hand.NaoHand.addCard(card.vs ,card.ss)
+  #check if Nao has won or gone over
+  if totalHand() == 21:
+    print("I win!")
+    #abs layer call
+  elif totalHand() > 21:
+      print("I lose")
+      #abs layer call
+  else:
+    print("I'm still playing")
+    #abs layer call
+
+#calcualte sum of all cards in hands
+def totalHand():
+  sum = 0
+  for card in hand.NaoHand:
+    #if card is jack, queen, king, or 10, it's value is 10
+    if card.value >= 10:
+      sum += 10
+    #if its an ace, see if making it 11 or 1 will work
+    elif card.vs = "a":
+      if sum + 11 <= 21:
+        sum += 11
+        print("ace as 11")
+      else:
+        sum += 1
+        print("ace as 1")
+    else:
+      sum += card.value
+  return sum
 
